216cf1e5c395ff4fe0a139805c1944a3502da852
xanik <xanikyan00@gmail.com>
2025-10-17 23:23:46 +0500

    Add project files.

---
+++ Tyuiu.KhanikyanDK.Sprint4.Task0.V22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Tyuiu.KhanikyanDK.Sprint4.Task0.V22.Lib;
+namespace Tyuiu.KhaniykanDK.Sprint4.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Ханикян Д. К. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Ханикян Давит Каренович | ИСТНб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму нечетных *");
+            Console.WriteLine("* элементов массива. {9, 5, 7, 4, 5, 3, 7, 8, 9, 1}                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int[] nums = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            double result = ds.GetSumOddArrEl(nums);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
