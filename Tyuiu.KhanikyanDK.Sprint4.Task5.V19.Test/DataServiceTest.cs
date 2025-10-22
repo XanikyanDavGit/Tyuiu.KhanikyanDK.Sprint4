a6c645f752895200a132152b1da89c0da19959c5
xanik <xanikyan00@gmail.com>
2025-10-22 23:07:37 +0500

    Task5

---
+++ Tyuiu.KhanikyanDK.Sprint4.Task5.V19.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KhanikyanDK.Sprint4.Task5.V19.Lib;
+
+namespace Tyuiu.KhanikyanDK.Sprint4.Task5.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] array =
+            {
+                { -2, -1,  0,  1,  2 },
+                { -2,  3,  1,  0, -1 },
+                {  2,  2, -2,  1,  0 },
+                {  3, -1, -2,  1,  2 },
+                {  0, -2,  3,  1, -1 }
+            };
+
+            int res = ds.Calculate(array);
+            int wait = 12; 
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
