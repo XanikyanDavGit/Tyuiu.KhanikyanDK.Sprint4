5d6129fc390bf5fe0072f0880af57399396753b8
xanik <xanikyan00@gmail.com>
2025-10-31 23:01:00 +0500

    Task7

--- Tyuiu.KhanikyanDK.Sprint4.Task7.V4/Program.cs
+++ Tyuiu.KhanikyanDK.Sprint4.Task7.V4/Program.cs
@@ -16,16 +16,36 @@ namespace Tyuiu.KhanikyanDK.Sprint4.Task7.V4
             Console.WriteLine("* Выполнил: Ханикян Давит Каренович | ИСТНб-25-1                          *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дана строка цифр \"382976421897\".                                      *");
+            Console.WriteLine("* Дана строка цифр \"382976421897\".                                       *");
             Console.WriteLine("* Нужно преобразовать её в матрицу 3x4 и посчитать сумму нечетных чисел.  *");
             Console.WriteLine("***************************************************************************");
-
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
 
             int n = 3;
             int m = 4;
-
-
             string value = "382976421897";
+
+            int[,] matrix = new int[n, m];
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[index].ToString());
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
 
             DataService ds = new DataService();
             int result = ds.Calculate(n, m, value);
@@ -33,7 +53,7 @@ namespace Tyuiu.KhanikyanDK.Sprint4.Task7.V4
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Сумма нечетных элементов = " + result);
+            Console.WriteLine("Сумма нечетных элементов массива = " + result);
             Console.WriteLine("***************************************************************************");
 
             Console.ReadKey();
